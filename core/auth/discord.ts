a3a7ce34d4b399131c1db9fab77a25fa524ef8d7
Gaev Mikhail <gaev.ma@skbkontur.ru>
2021-07-20 21:36:13 +0500

    Update discord.ts

--- core/auth/discord.ts
+++ core/auth/discord.ts
@@ -11,13 +11,13 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
 const redirect = encodeURIComponent(`${process.env.REDIRECT}`);
 
 discord.get("/auth", (_:Request, res:Response) => {
-    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=667735586169159694&redirect_uri=${redirect}&response_type=code&scope=identify%20guilds`)
+    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=667735586169159694&redirect_uri=https%3A%2F%2Fdsbotweb.herokuapp.com%2Fapi%2Fdiscord%2Fcallback&response_type=code&scope=identify%20guilds`)
 })
 
 discord.get('/callback', catchAsyncErrors(async (req:Request, res:Response) => {
     if (!req.query.code) throw new Error('NoCodeProvided');
     const code = req.query.code.toString();
-    const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
+    const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=https%3A%2F%2Fdsbotweb.herokuapp.com%2Fapi%2Fdiscord%2Fcallback`,
         {
             method: 'POST',
             body: new URLSearchParams({
